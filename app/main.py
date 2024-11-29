1c6e8e16344f6489f59b18680494917e28d6ddef
David Rambo <davidrambo@mailfence.com>
2024-11-28 17:26:33 -0800

    stubs for input and output models

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 """Text parser for adding multiple gift ideas in a single string."""
 
 import fastapi
+import pydantic
 
 
 app = fastapi.FastAPI()
+
+
+class InputText(pydantic.BaseModel):
+    pass
+
+
+class ParsedJSON(pydantic.BaseModel):
+    pass
